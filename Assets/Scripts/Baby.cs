4bf45eae1a1047491821740a00b095fcc456183b
Anakin Trotter <anakin.trotter@gmail.com>
2021-10-01 23:28:35 -0700

    Added baby timer

--- Assets/Scripts/Baby.cs
+++ Assets/Scripts/Baby.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class Baby : MonoBehaviour
 {
+    public float cryInterval = 10f;
+    private float timeLeft;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeLeft = cryInterval;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timeLeft > 0) {
+            timeLeft -= Time.deltaTime;
+        } else {
+            Cry();
+            float newTime = cryInterval - Globals.level;
+            timeLeft = newTime >= 5 ? newTime : 5;
+        }
+    }
+
+    void Cry()
+    {
+        Globals.debuffs.Clear();
+        // possible stuff to do:
+        // give the player a debuff until the next tantrum
+        // trigger a change in the house
     }
 
     void OnTriggerEnter2D(Collider2D col)
--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class Globals {
-    public static int level = 1; // counts the level for difficulty scaling
+    public static int level = 0; // counts the level for difficulty scaling
     public static int pacifiers = 0; // counts how many pacifiers the player has
 
     // keep track of player debuffs
