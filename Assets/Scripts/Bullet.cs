097e07e33709d9c09876dc1ac303029a992bf8e4
Anakin Trotter <anakin.trotter@gmail.com>
2021-10-02 13:51:32 -0700

    Fixed cannon

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,25 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bulletscript : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
-    public float speed;
+    public static float speed = 10f;
     Rigidbody2D rb;
-    public Transform player;
-    public Transform cannon;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        if(player.position.x > cannon.position.x)
-            rb.velocity = transform.right*speed;
-        else
-            rb.velocity = transform.right*speed*-1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public Rigidbody2D getRB()
+    {
+        return rb;
     }
 }
