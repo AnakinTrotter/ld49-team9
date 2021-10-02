59200cfbdc8750c44472b578c3a0c6bca8be0024
Anakin Trotter <anakin.trotter@gmail.com>
2021-10-01 21:35:44 -0700

    Added slow debuff

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -7,6 +7,6 @@ public static class Globals {
     public static int pacifiers = 0; // counts how many pacifiers the player has
 
     // keep track of player debuffs
-    public enum DebuffState { invert } 
+    public enum DebuffState { invert, slow } 
     public static HashSet<DebuffState> debuffs = new HashSet<DebuffState>();
 }
