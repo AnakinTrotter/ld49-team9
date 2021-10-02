cc4fe0437943471cc98931dcab642bf9d56d9209
Anakin Trotter <anakin.trotter@gmail.com>
2021-10-02 16:49:58 -0700

    Added moving platform

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -153,6 +153,7 @@ public class PlayerController : MonoBehaviour
 
     private bool IsGrounded()
     {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+        return Physics2D.BoxCast(coll.bounds.center, new Vector2(coll.bounds.size.x * 0.75f, 
+            coll.bounds.size.y), 0f, Vector2.down, .1f, jumpableGround);
     }
 }
