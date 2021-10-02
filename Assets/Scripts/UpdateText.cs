f6bb38063e8a0e99601f6e327adb09962e33808d
Jeffrey Shen <jeffreyshen859@gmail.com>
2021-10-02 15:08:53 -0700

    Added basic UI for status (i.e. lives, health, etc.)

---
+++ Assets/Scripts/UpdateText.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateText : MonoBehaviour
+{
+    public Text statusText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        statusText = GetComponent<Text>();
+        statusText.text = "starting...";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        statusText.text = $"Level: {Globals.level.ToString()}   Lives: {Globals.lives.ToString()}   Pacifiers: {Globals.pacifiers.ToString()}";
+    }
+}
