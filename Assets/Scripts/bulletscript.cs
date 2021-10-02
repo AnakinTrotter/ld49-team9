e57dfc67df895ba62ca76ae7b88c1cfa26b8f7d3
Matthew <matthewjwang5@gmail.com>
2021-10-02 01:43:30 -0700

    fixed cannon to shoot forward and backward

--- Assets/Scripts/bulletscript.cs
+++ Assets/Scripts/bulletscript.cs
@@ -6,11 +6,16 @@ public class bulletscript : MonoBehaviour
 {
     public float speed;
     Rigidbody2D rb;
+    public Transform player;
+    public Transform cannon;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = transform.right*speed;
+        if(player.position.x > cannon.position.x)
+            rb.velocity = transform.right*speed;
+        else
+            rb.velocity = transform.right*speed*-1;
     }
 
     // Update is called once per frame
