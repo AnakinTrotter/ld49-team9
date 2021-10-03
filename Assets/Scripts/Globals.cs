bdce28e2eb77b053e787b6b48f9bbff6396787a4
Anakin Trotter <anakin.trotter@gmail.com>
2021-10-02 18:50:29 -0700

    Update Globals.cs

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -13,7 +13,7 @@ public static class Globals {
     public static int maxNumPacifiers = 1;  // maximum number of pacifiers to exist in the level
 
     // keep track of player debuffs
-    public enum DebuffState { invert, slow, moon, fast, rewind } 
+    public enum DebuffState { invert, slow, fast, moon, rewind } 
     public static HashSet<DebuffState> debuffs = new HashSet<DebuffState>();
 
     public static void gameOver() {
