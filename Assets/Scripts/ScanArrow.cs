c05fb905ce0050b643872a0b750610147823bca7
cm600286 <91765679+cm600286@users.noreply.github.com>
2021-10-02 21:40:47 -0700

    Plz work
    Pray nothing bad happens

--- Assets/Scripts/ScanArrow.cs
+++ Assets/Scripts/ScanArrow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ScanArrow : MonoBehaviour
 {
     public Transform pacifier, parent;
+    private float alpha, delay = 2f, fadeSpeed = 0.02f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,27 @@ public class ScanArrow : MonoBehaviour
             return;
         }
         Debug.DrawLine(pacifier.position, parent.position);
+        orient();
+
+        delay -= Time.deltaTime;
+        if (delay <= 0)
+        {
+            setAlpha(alpha - fadeSpeed);
+        }
+
+    }
+
+    private void setAlpha(float a)
+    {
+        alpha = Mathf.Max(0, a);
+        SpriteRenderer sprite = this.GetComponent<SpriteRenderer>();
+        Color color = sprite.color;
+        color.a = alpha;
+        sprite.color = color;
+    }
+
+    private void orient()
+    {
         Vector3 dir = pacifier.position - parent.position;
         this.transform.position = parent.position + dir / dir.magnitude * 2;
         this.transform.rotation = Quaternion.Euler(0,0, Mathf.Rad2Deg * Mathf.Atan(dir.y / dir.x));
